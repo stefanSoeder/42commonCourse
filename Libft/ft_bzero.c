258ff06557b39a89ace58f843d477cdd32ab6b96
stefan42 <stemarti@c2r14s5.42madrid.com>
2024-09-24 18:58:37 +0200

    bzero

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 14:59:31 by stemarti          #+#    #+#             */
-/*   Updated: 2024/09/24 18:56:13 by stemarti         ###   ########.fr       */
+/*   Created: 2024/09/24 18:33:22 by stemarti          #+#    #+#             */
+/*   Updated: 2024/09/24 18:55:52 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stddef.h>
-# include <string.h>
-# include <stdlib.h>
-
-int					ft_isalpha(int c);
-int					ft_isdigit(int c);
-int					ft_isalnum(int c);
-int					ft_isascii(int c);
-int					ft_isprint(int c);
-size_t				ft_strlen(const char *s);
-void				*ft_memset(void *s, int c, size_t n);
-void				ft_bzero(void *s, size_t n);
-
-#endif
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
