1b1580d3a29813a667e105af4c56a2f1fb92edb0
Stefan Miguel Martin Goeder <stemarti@c2r13s6.42madrid.com>
2024-09-24 13:09:16 +0200

    strlen

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 14:59:31 by stemarti          #+#    #+#             */
-/*   Updated: 2024/09/24 13:05:58 by stemarti         ###   ########.fr       */
+/*   Created: 2024/09/24 13:01:53 by stemarti          #+#    #+#             */
+/*   Updated: 2024/09/24 13:08:33 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int	ft_strlen(char *s)
+{
+	int	i;
 
-# include <unistd.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_strlen(char *s);
-
-#endif
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
