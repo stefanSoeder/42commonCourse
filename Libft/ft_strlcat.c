01598db794505d1981864aed6c3a6f9642ee9617
stefanSoeder <stefanms3@gmail.com>
2024-09-26 17:45:01 +0200

    strlcat_still_not_working

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,25 +6,29 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 12:33:38 by stemarti          #+#    #+#             */
-/*   Updated: 2024/09/26 13:28:54 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/09/26 17:44:18 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	const int	b = ft_strlen(dst);
+	size_t	original_len;
 	size_t	i;
 	size_t	j;
 
-	i = 0;
+	original_len = ft_strlen(dst);
+	i = original_len;
 	j = 0;
-	while(dst[i] != '\0')
-		i++;
-	while((src[j] != '\0') && (j < size))
-	{	
-		dst[i + 1] = src [j];
-		j++;
+	if (size >= original_len)
+	{
+		while ((src[j] != '\0') && ((i <= (size - 1))))
+		{
+			dst[i] = src[j];
+			i++;
+			j++;
+		}
+		dst[i] = '\0';
 	}
-	return ((b + (ft_strlen(src - size))));
+	return ((original_len + (ft_strlen(src))));
 }
