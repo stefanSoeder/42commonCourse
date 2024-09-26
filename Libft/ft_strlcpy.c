16d9ad43a390bca01c5cc195130a26ae3c756c5f
stefanSoeder <stefanms3@gmail.com>
2024-09-26 12:21:46 +0200

    strlcpy

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,25 +6,29 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:42:45 by stemarti          #+#    #+#             */
-/*   Updated: 2024/09/25 17:17:25 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/09/26 12:20:41 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	if (size)
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while ((src[i] != '\0'))
+		i++;
+	if (size != 0)
 	{
-		size_t	i;
-		size_t	j;
-
-		i = 0;
-		j = 0;
-		while(dest[i] != '\0')
-			i++;
-		while
-			 
-
+		while ((src[j] != '\0') && (j < (size -1)))
+		{
+			dest[j] = src[j];
+			j++;
+		}
+		dest[j] = '\0';
 	}
+	return (i);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:59:31 by stemarti          #+#    #+#             */
-/*   Updated: 2024/09/25 15:49:27 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/09/26 12:12:09 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
+size_t				ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
