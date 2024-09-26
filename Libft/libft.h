59a040be0429bb8969ba42564e8e7a7c1ff0d594
stefanSoeder <stefanms3@gmail.com>
2024-09-26 18:16:59 +0200

    tolower

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:59:31 by stemarti          #+#    #+#             */
-/*   Updated: 2024/09/26 18:09:35 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:15:06 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,6 @@ void				*ft_memmove(void *dest, const void *src, size_t n);
 size_t				ft_strlcpy(char *dest, const char *src, size_t size);
 //size_t				ft_strlcat(char *dst, const char *src, size_t size);
 int					ft_toupper(int c);
+int					ft_tolower(int c);
+
 #endif
