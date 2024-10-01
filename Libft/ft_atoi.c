e6ae7ff89748bdf3be40d577c1394933f1659bb0
stefanSoeder <stefanms3@gmail.com>
2024-10-01 13:26:14 +0200

    atoi_not_working

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,11 +6,13 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 11:16:45 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/01 11:58:19 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/01 13:21:47 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_isspace(int c);
 
 int	ft_atoi(const char *nptr)
 {
@@ -22,24 +24,29 @@ int	ft_atoi(const char *nptr)
 	sign = 1;
 	result = 0;
 	while (nptr[i] != '\0')
-		if(ft_isspace(nptr[i]))
+	{
+		while(ft_isspace(nptr[i]))
 			i++;
 		if((nptr[i] == '+') && (nptr[i + 1] != '-'))
 			i++;
 		if(nptr[i] == '-')
 		{
-			sing = -1;
+			sign = -1;
 			i++;
 		}
-		if((nptr[i] >= '0') && (nptr[i] <= '9'))
-
-
+		while((nptr[i] >= '0') && (nptr[i] <= '9'))
+		{
+			result = (result * 10) + (nptr[i] - '0');
+			i++;
+		}
+		i++;
+	}
+	return (result * sign);
 }
 
 int	ft_isspace(int c)
 {
-	if ((c >= 9) || (c >= 13) || (c == 32)
+	if ((c >= 9) || (c >= 13) || (c == 32))
 		return (1);
 	return (0);
 }
-	
