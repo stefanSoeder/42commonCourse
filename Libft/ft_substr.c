ff8d1dab96a2245e87a7f38080c11ee5964a543a
stefanSoeder <stefanms3@gmail.com>
2024-10-02 18:28:44 +0200

    substr_working_with_errors

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:55:12 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/02 18:13:17 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/02 18:26:48 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char const	*sub;
+	char	*sub;
 
 	i = 0;
 	j = 0;
-	sub = (char const *)malloc(ft_strlen(len + 1));
+	sub = (char *)malloc(len + 1);
 	if (!sub)
 		return (NULL);
 	while (s[i])
 	{
 		i++;
-		if(s[i] == (size_t)start)
+		if(i == (size_t)start)
 		{
-			while(s[j] <= len)
+			while(j <= len)
 			{
-				sub[j] = s[j];
-				j++:
+				sub[j] = s[j + i];
+				j++;
 			}
 			sub[j] = '\0';
 		}
-		return (sub);
 	}
+	return (sub);
 }
