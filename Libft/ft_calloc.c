a9aa2e2e99e3d42c04fb3557d62409eb47edc143
stefanSoeder <stefanms3@gmail.com>
2024-10-02 17:05:03 +0200

    calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:40:56 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/01 18:50:15 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/02 17:03:57 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
+	unsigned char	*alloc;
 	
+	alloc = (void *)malloc(nmemb * size);
+	if (!alloc)
+		return (0);
+	ft_bzero(alloc, nmemb * size);
+	return (alloc);
 }
