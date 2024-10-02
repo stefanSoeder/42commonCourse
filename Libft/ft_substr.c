3b823ea015c045e2e64a53cdacb43de630b9db83
stefanSoeder <stefanms3@gmail.com>
2024-10-02 19:20:52 +0200

    substr_segfault

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:55:12 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/02 18:26:48 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:08:02 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,23 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	j = 0;
+	if (!s)
+		return (NULL);
 	sub = (char *)malloc(len + 1);
 	if (!sub)
 		return (NULL);
 	while (s[i])
 	{
-		i++;
-		if(i == (size_t)start)
+		if (i == (size_t)start)
 		{
-			while(j <= len)
+			while (j < len)
 			{
 				sub[j] = s[j + i];
 				j++;
 			}
 			sub[j] = '\0';
 		}
+		i++;
 	}
 	return (sub);
 }
