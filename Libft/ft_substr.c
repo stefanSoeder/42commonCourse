5f7f25ff831118c4c1c119484e9f6c9add89ca13
stefanSoeder <stefanms3@gmail.com>
2024-10-07 11:56:14 +0200

    substr_working

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -1,35 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 11:51:50 by stemarti          #+#    #+#             */
+/*   Updated: 2024/10/07 11:54:43 by stemarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	size_t	j;
 	char	*sub;
 
 	i = 0;
-	j = 0;
 	if (!s)
 		return (NULL);
 	if (ft_strlen(s) < start)
 		return (ft_strdup(""));
-	if ((start + len) > (ft_strlen(s)))
+	if (len > (ft_strlen(s + start)))
 		len = ft_strlen(s + start);
-	sub = malloc(sizeof (char) *(len + 1));
+	sub = ft_calloc(len + 1, sizeof(char));
 	if (!sub)
 		return (NULL);
-	while (s[i])
-	{
-		if (i == (size_t)start)
-		{
-			while (j < len)
-			{
-				sub[j] = s[j + i];
-				j++;
-			}
-			sub[j] = '\0';
-		}
-		i++;
-	}
+	while (i < len)
+		sub[i++] = s[start++];
 	return (sub);
 }
