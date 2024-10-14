fbe81f7c0349fe65888eb9522f57b1fce3c69341
stefanSoeder <stefanms3@gmail.com>
2024-10-14 18:50:58 +0200

    strmapi

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:59:31 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/14 17:10:05 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/14 18:37:54 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,6 @@ char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
 char				**ft_split(const char *s, char c);
 char				*ft_itoa(int c);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
 #endif
