94b419b1a19e5c92438a90806a63a528ebb733c4
stefanSoeder <stefanms3@gmail.com>
2024-10-16 11:18:20 +0200

    ts_update

--- Tests/Libft/ts_memcpy.c
+++ Tests/Libft/ts_memcpy.c
@@ -6,22 +6,41 @@
 /*   By: stemarti <stemarti@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 19:56:26 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/15 21:29:44 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/16 10:56:12 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ts_memcpy(void *dest, const void *src, size_t n))
+void	*ts_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	
+	if (!dest || !src || (n == 0))
+		return (dest);
 	while (i < n)
 	{
-		(((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		(((unsigned char *)dest)[i] = ((unsigned char *)src)[i]);
 		 i++;
-		 }
-	return (dest)
+	}
+	return (dest);
+}
+
+int	main(void)
+{
+	char	*src;
+	char	*dest;
+	size_t	n;
+
+	src = "Hola Mundo";
+	dest = "Palo";
+	printf("Before ts_memcpy:\n");
+    	printf("Source: %s\n", src);
+    	printf("Destination: %s\n\n", dest);
+	ts_memcpy(src, dest, b);
+	printf("After ts_memcpy:\n");
+    	printf("Source: %s\n", src);
+    	printf("Destination: %s\n", dest);
+	return (0);
 }
