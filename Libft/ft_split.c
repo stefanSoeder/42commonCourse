5c30b7ae75cdd8b625602e8e48d16517632ba305
stefanSoeder <stefanms3@gmail.com>
2024-10-17 17:57:25 +0200

    All_Up_&_Running

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,97 +6,88 @@
 /*   By: stemarti <stemarti@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:35:29 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/15 11:14:12 by soeder85         ###   ########.fr       */
+/*   Updated: 2024/10/17 17:54:39 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_word_counter(char const *s, char c);
-static int	ft_word_len(const char *s, int start, char c);
-static void	ft_free(char **arrays_array);
-static void	ft_array_creator(char const *s, char **arrays_array, char c);
+static void	ft_free(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+static int	ft_count_words(const char *str, char c)
+{
+	int	i;
+	int	count;
+	int	in_word;
+
+	i = 0;
+	count = 0;
+	in_word = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			in_word = 0;
+		else if (!in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+static char	**populate_split(char const *s, char c, char **split)
+{
+	size_t	start;
+	size_t	i;
+	size_t	j;
+
+	start = 0;
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+		{
+			split[j] = ft_substr(s, start, i - start + 1);
+			if (!split[j])
+			{
+				ft_free(split);
+				return (NULL);
+			}
+			j++;
+		}
+		if (s[i] == c && s[i + 1] != c)
+			start = i + 1;
+		i++;
+	}
+	split[j] = NULL;
+	return (split);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	char	**arrays_array;
+	char	**split;
 
 	if (!s)
 		return (NULL);
-	arrays_array = ft_calloc ((ft_word_counter(s, c) + 1), sizeof(char *));
-	if (!arrays_array)
+	split = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!split)
 		return (NULL);
-	ft_array_creator (s, arrays_array, c);
-	return (arrays_array);
-}
-
-static int	ft_word_counter(char const *s, char c)
-{
-	int	i;
-	int	words;
-
-	i = 0;
-	words = 0;
-	while (s[i] != '\0')
-	{
-		if (((s[i] != c)) && ((i == 0) || (s[i - 1] == (char) c)))
-			words++;
-		i++;
-	}
-	return (words);
-}
-
-static int	ft_word_len(const char *s, int start, char c)
-{
-	int	len;
-
-	len = 0;
-	while ((s[start] != c) && (s[start] != '\0'))
-	{
-		len++;
-		start++;
-	}
-	return (len);
-}
-
-static void	ft_free(char **arrays_array)
-{
-	int	k;
-
-	k = 0;
-	if (!arrays_array)
-		return ;
-	while (arrays_array[k])
-	{
-		free(arrays_array[k]);
-		k++;
-	}
-	free(arrays_array);
-}
-
-static void	ft_array_creator(char const *s, char **arrays_array, char c)
-{
-	int		i;
-	int		k;
-	char	*new_word;
-
-	i = 0;
-	k = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] != c)
-		{
-			new_word = ft_substr(s, i, ft_word_len(s, i, c));
-			if (!new_word)
-			{
-				ft_free(arrays_array);
-				return ;
-			}
-			arrays_array[k] = new_word;
-			k++;
-			i += ft_word_len(s, i, c);
-		}
-		else
-			i++;
-	}
+	split = populate_split(s, c, split);
+	return (split);
 }
