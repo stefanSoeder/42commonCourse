10036e41a79e36932d9faf8a3e64ea9dfeb3adde
stefanSoeder <stefanms3@gmail.com>
2024-10-23 16:32:50 +0200

    printf_lib_correction

--- Printf/ft_printf.h
+++ Printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:59:31 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/23 16:20:32 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:25:24 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 # include <unistd.h>
 # include <stdio.h>
-# include <stddef.h>
 # include <string.h>
 # include <stdlib.h>
 
 void				ft_putchar(char c);
 void				ft_puts(char *s);
 void				ft_putptr(void *ptr);
+
 #endif
