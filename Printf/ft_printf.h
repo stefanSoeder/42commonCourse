5f3503483245ebb3306b5d3e2bd56b5008330bcf
stefanSoeder <stefanms3@gmail.com>
2024-10-23 18:15:06 +0200

    ft_puthex

--- Printf/ft_printf.h
+++ Printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: stemarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:59:31 by stemarti          #+#    #+#             */
-/*   Updated: 2024/10/23 17:37:04 by stemarti         ###   ########.fr       */
+/*   Updated: 2024/10/23 18:14:40 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_PRINTF_H
 
 # include <unistd.h>
-# include <stdio.h>
+# include <stdarg.h>
 # include <string.h>
 # include <stdlib.h>
 
@@ -23,5 +23,6 @@ void				ft_puts(char *s);
 void				ft_putptr(void *ptr);
 void				ft_putnbrprint(int n);
 void				ft_putunsignnbr(unsigned int n);
+void				ft_puthex(unsigned long long int n, int cap);
 
 #endif
---
+++ Printf/ft_puthex.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stemarti <stemarti@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 17:46:30 by stemarti          #+#    #+#             */
+/*   Updated: 2024/10/23 18:11:47 by stemarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_puthex(unsigned long long num, int cap)
+{
+	int		len;
+	char	hex_lower;
+	char	hex_upper;
+
+	len = 0;
+	hex_lower = "0123456789abcdef"[num % 16];
+	hex_upper = "0123456789ABCDEF"[num % 16];
+	if (num >= 16)
+		len += ft_puthex(num / 16, cap);
+	if(!cap)
+		len += ft_putchar(hex_lower);
+	else
+		len += ft_putchar(hex_upper);
+	return (len);
+}
