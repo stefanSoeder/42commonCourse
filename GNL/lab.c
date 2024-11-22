676578a7dc5aeaaf502b579e2f331f106d031cbc
stefanSoeder <stefanms3@gmail.com>
2024-11-22 20:15:55 +0100

    basics

--- GNL/lab.c
+++ GNL/lab.c
@@ -1,32 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tests.c                                            :+:      :+:    :+:   */
+/*   lab.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stemarti <stemarti@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 18:46:13 by stemarti          #+#    #+#             */
-/*   Updated: 2024/11/14 11:35:55 by stemarti         ###   ########.fr       */
+/*   Created: 2024/11/22 17:01:39 by stemarti          #+#    #+#             */
+/*   Updated: 2024/11/22 20:09:44 by stemarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 //function that reads BUFFER_SIZE bytes
-/*23char	*read_excerpt(int fd)
+char	*read_excerpt(int fd)
 {
-	char *buf;
+	char	*buf;
 	ssize_t read_bytes;
 
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if(!buf)
 		return(ft_free(&(buf)));
-	fd = open("test_text.txt", O_RDNLY);
+	fd = open("test_text.txt", O_RDONLY);
 	if (fd == -1)
-	{
-		perror("Not able to open file");
 		return (1);
-	}
 	read_bytes = read(fd, buf, ft_strlen(buf));
 	if (read_bytes == -1)
 	{
@@ -35,9 +32,34 @@
 		return (1);
 	}
 	buf[read_bytes] = '\0';
-	
-}*/
+	return (buf);
+}
+// Now we want to check if there's '\n' or not, and get what's before & after thati, if there's no \n we add everyhing to line. If there's a \n. we add everything up to \n to line and we return line.
 
+char	*excerpt_analyzer(char *read_excerpt)
+{
+	static char		*line;
+	
+	if (ft_strchr(read_excerpt, '\n') == 1)
+}
+
+char	get_nex_line(int fd)
+{
+	char	*excerpt;
+	static char	line;
+	
+	excerpt = read_excerpt(fd);
+	if(ft_strchr(excerpt, '\n') == 1)
+	while (ft_strchr(excerpt, '\n') == 0)
+	{
+		read_excerpt(fd);
+		int	i;
+
+		while(excerpt[i] != '\n')
+			i++;
+	}
+
+}
 // So, the main will call the function that gets the next line.
 // That function has to read a file, and get line by line all its content.
 // It will have to read BUFFER_SIZE bytes of the text.
@@ -51,14 +73,9 @@
 
 // I will free everything after buffer[bytes_read +1] in order to control memory flow:w
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-
 int main() {
-    int fd;
-    char buffer[5];
+    /*int fd;
+    char buffer[BUFFER_SIZE];
     ssize_t bytes_leidos;
 
     // Abrir el archivo
@@ -79,7 +96,7 @@ int main() {
     }
 
     close(fd);
-    return 0;
+    return 0;/*
 }
 
 
